991b3293042a545a293227324b77a74ab83dcaf5
meltemod <35743173+meltemod@users.noreply.github.com>
2020-01-03 13:39:22 -0500

    Add files via upload

---
+++ Optional_R_basics_with_swirl.R
@@ -0,0 +1,63 @@
+##---------------------------------------##
+##                                       ##
+##      R for Stats                      ##
+##                                       ##
+##---------------------------------------##
+
+##Author: Meltem Odabas
+##Date: 2019-12-10
+
+#==========================================================#
+#                                                          #
+#     Chapter 1: Learning R Basics With Swirl              #
+#                                                          #
+#==========================================================#
+
+#to install the package, please run the code below:
+#(to run the code, simply click on the line, or select the whole line, and then hit Ctrl+enter or Apple+enter)
+install.packages("swirl")
+
+#this way you install the package to your computer. Once you did it, you do not need to install the same
+#package every time you reopen R or Rstudio. But, you will need to type library() to be able to use the package 
+#every time you reopen your R / RStudio. 
+
+#So, let's load the swirl library using library()
+
+library(swirl)
+
+#OMG, swirl says hi! So, when you are ready to start learning new lessons, just type swirl()
+
+swirl()
+
+#Then swirl will ask what it should call you... What is super nice about swirl is that it kind of talks to you while
+#you are learning R. 
+
+#swirl will give you some prompts that explains how to use it. 
+#For your reference, I will copy-paste those lines at the end of this Rscript. You are welcome!
+
+#so you can use swirl as your reference to practice R. The first lesson, R Programming, for instance,
+#shows how to save your workspace, how to create number sequences, vector, dataframes, and such. 
+
+###############################
+#SWIRL INSTRUCTIONS
+
+#First of all, you
+#| should know that when you see '...', that means you should press Enter when you are done reading and ready to
+#| continue.
+
+#| Also, when you see 'ANSWER:', the R prompt (>), or when you are asked to select from a list, that means it's your turn
+#| to enter a response, then press Enter to continue.
+
+#| You can exit swirl and return to the R prompt (>) at any time by pressing the Esc key. If you are already at the
+#| prompt, type bye() to exit and save your progress. When you exit properly, you'll see a short message letting you know
+#| you've done so.
+
+#| When you are at the R prompt (>):
+#  | -- Typing skip() allows you to skip the current question.
+#| -- Typing play() lets you experiment with R on your own; swirl will ignore what you do...
+#| -- UNTIL you type nxt() which will regain swirl's attention.
+#| -- Typing bye() causes swirl to exit. Your progress will be saved.
+#| -- Typing main() returns you to swirl's main menu.
+#| -- Typing info() displays these options again.
+
+
